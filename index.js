5badfdee412aecd3af61237ae9d7f3a03bad178d
Vitalis Ian <viterlis254@gmail.com>
2024-06-27 11:57:46 +0300

    adds index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(){
+}
+function receivesAFunction(){
+}
+function receivesAFunction(ideal){
+ideal()
+}
+function returnsANamedFunction() {
+    return function namedFunction() {
+    };
+  }
+  function returnsAnAnonymousFunction() {
+    return function() {
+    };
+  }
